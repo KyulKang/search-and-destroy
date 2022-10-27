935ba39415c1f7f835e9da70f828f399ab431fe0
Kyul Kang <warp2xk@gmail.com>
2022-10-27 17:43:16 -0400

    completed isLoop

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,9 +2,33 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  const head = linkedlist.head;
 
+  let slow = head;
+  let fast = head;
+
+  //   let value = 0;
+
+  //   switch (true) {
+  //     case !head || !head.next:
+  //       return false;
+  //     case head.next === head:
+  //       return true;
+  //   }
+  //if (!head || !head.next) return false;
+  //if (head.next === head) return true;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +38,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
